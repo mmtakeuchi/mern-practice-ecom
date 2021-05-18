15d7290def0fa610290fa4240c8cb219f78464c5
mmtakeuchi <mikemtaki@gmail.com>
2021-05-18 12:45:39 -0700

    create store and actions for redux

---
+++ frontend/frontend/src/store.js
@@ -0,0 +1,19 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+
+const intitialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+  rootReducer,
+  intitialState,
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX__DEVTOOLS_EXTENSION__ &&
+      window.__REDUX__DEVTOOLS_EXTENSION__()
+  )
+);
+
+export default store;
